f43ac7e99f8e8c8507c46c4bd815ed11c48f2a6d
John Doe <saullainez@hotmail.es>
2018-09-26 12:21:13 -0600

    m

--- web/php/conexion.php
+++ web/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
     $link = mysqli_connect ("localhost:3306", "gamerchallenge@localhost", "");
-	mysqli_select_db( $link, "gamercha_chat2" );
+	mysqli_select_db( $link, "gamercha chat2" );
     if(mysqli_connect_errno())
     {
         printf("conexion fallida", mysqli_connect_error());
