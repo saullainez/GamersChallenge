c2d66bb7b0104108dd65045eec7ce49c292e97ea
John Doe <saullainez@hotmail.es>
2018-09-26 11:39:13 -0600

    Cambiar usuario de la conexion a la BBDD

--- web/php/conexion.php
+++ web/php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect ("198.15.102.82", "gamerchallenge@localhost", "");
+    $link = mysqli_connect ("198.15.102.82", "root", "");
 	mysqli_select_db( $link, "gamercha_chat2" );
     if(mysqli_connect_errno())
     {
